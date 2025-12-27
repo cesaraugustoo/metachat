6b2cd3fd14e26948178b7e9b21e81e9f7451deeb
cesaraugustoo <cesar.augusto.gomes.souza@gmail.com>
2025-12-27 10:16:04 -0300

    feat(models): Export VLLMModel from models package

--- metachat_core/core/models/__init__.py
+++ metachat_core/core/models/__init__.py
@@ -3,5 +3,6 @@ from .openai import OpenAIModel
 from .anthropic import AnthropicModel
 from .llama import LlamaModel
 from .openrouter import OpenRouterModel
+from .vllm import VLLMModel
 
-__all__ = ["BaseModel", "LLMResponse", "OpenAIModel", "AnthropicModel", "LlamaModel", "OpenRouterModel"]
+__all__ = ["BaseModel", "LLMResponse", "OpenAIModel", "AnthropicModel", "LlamaModel", "OpenRouterModel", "VLLMModel"]
